776f2559c10e05d82f335fb7bc74a6c91dcc63f3
Jonathan Wong <jonathan.wong00@yahoo.com>
2026-02-13 16:09:29 -0800

    organize components

--- include/dynamic_foam/sim2d/components.h
+++ include/dynamic_foam/sim2d/components.h
@@ -3,70 +3,69 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/quaternion.hpp>
 
-namespace dynamic_foam::sim2d {
+namespace DynamicFoam::Sim2D {
+    // ============================================================================
+    // Rigid Body Components
+    // ============================================================================
 
-// ============================================================================
-// Rigid Body Components
-// ============================================================================
+    // Persistent Components
+    struct CenterOfMass {
+        glm::vec3 value{0.0f};
+    };
+    struct InertiaTensor {
+        glm::mat3 value{1.0f}; // Identity matrix by default
+    };
 
-struct Position {
-    glm::vec3 value{0.0f};
-};
+    // Transient Components
+    struct Position {
+        glm::vec3 value{0.0f};
+    };
+    struct Velocity {
+        glm::vec3 value{0.0f};
+    };
+    struct Orientation {
+        glm::quat value{1.0f, 0.0f, 0.0f, 0.0f}; // Identity quaternion (w, x, y, z)
+    };
+    struct AngularVelocity {
+        glm::vec3 value{0.0f};
+    };
+    struct AABB {
+        glm::vec3 min{0.0f};
+        glm::vec3 max{0.0f};
+    };
 
-struct Velocity {
-    glm::vec3 value{0.0f};
-};
+    // Persistent Foam Types
+    struct Static {};
+    struct Dynamic {};
+    struct Controller {};
 
-struct Orientation {
-    glm::quat value{1.0f, 0.0f, 0.0f, 0.0f}; // Identity quaternion (w, x, y, z)
-};
+    // ============================================================================
+    // Particle Components
+    // ============================================================================
 
-struct AngularVelocity {
-    glm::vec3 value{0.0f};
-};
+    // Persistent Components
+    struct ParticleLocalPosition {
+        glm::vec3 value{0.0f};
+    };
+    struct ParticleColor {
+        glm::vec3 rgb{1.0f}; // White by default
+    };
+    struct ParticleOpacity {
+        float value{1.0f};
+    };
 
-struct CenterOfMass {
-    glm::vec3 value{0.0f};
-};
+    // Transient Components
+    struct ParticleWorldPosition {
+        glm::vec3 value{0.0f};
+    };
+    struct ParticleMass {
+        float value{1.0f};
+    };
+    struct Surface {};
 
-struct InertiaTensor {
-    glm::mat3 value{1.0f}; // Identity matrix by default
-};
+    // Persistent Particle Types
+    struct Stencil {};
+    struct Mutable {};
+    struct Immutable {};
 
-struct Controller {};
-
-// Mutually exclusive physics properties
-struct Static {};
-struct Dynamic {};
-
-// ============================================================================
-// Particle Components
-// ============================================================================
-
-struct ParticleWorldPosition {
-    glm::vec3 value{0.0f};
-};
-
-struct ParticleLocalPosition {
-    glm::vec3 value{0.0f};
-};
-
-struct ParticleColor {
-    glm::vec3 rgb{1.0f}; // White by default
-};
-
-struct ParticleOpacity {
-    float value{1.0f};
-};
-
-struct ParticleMass {
-    float value{1.0f};
-};
-
-// Mutually exclusive topological properties
-struct Cutter {};
-struct Cuttable {};
-
-struct Surface {};
-
-} // namespace dynamic_foam::sim2d
+}
