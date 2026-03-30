ec9bc14cb690572a162fe66c49b7da245918e33e
Jonathan Wong <jonathan.wong00@yahoo.com>
2026-03-30 14:30:50 -0700

    discard extraneous exe

--- include/cuda_kernels.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-// CUDA kernel wrapper functions
-float* allocateDeviceMemory(size_t size);
-void freeDeviceMemory(float *d_ptr);
-void addArrays(float *d_c, const float *d_a, const float *d_b, int n);
